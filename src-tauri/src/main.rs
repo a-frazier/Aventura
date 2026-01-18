f7ffdb65b9ac5f96dc66e93bd85450894a1be54f
Failerko <c.dann@seznam.cz>
2026-01-18 15:52:08 +0100

    fix: appimage not launching

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Fix for "Failed to create GBM buffer" error on some Linux environments
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     aventura_lib::run()
 }
